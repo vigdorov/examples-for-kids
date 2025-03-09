3f8e4849f11cc74e915dc5de21b896ece808dbc4
Николай Вигдоров <vigdorov@MacBook-Air-Nikolaj.local>
2025-03-09 12:49:46 +0300

    fix reports

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
         '^.+\\.(js|jsx)$': 'babel-jest',
     },
     reporters: ['default', 'jest-junit'],
+    coverageReporters: ['cobertura', 'lcov', 'text'],
+    collectCoverageFrom: ['src/**/*.{js,jsx}', '!src/**/*.test.{js,jsx}', '!src/index.js', '!src/reportWebVitals.js'],
+    coverageDirectory: 'coverage',
 };
